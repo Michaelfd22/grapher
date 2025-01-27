d9d830c0bbb4074ce397652088206465a31e66a8
Michael <2121401254@k12.hi.us>
2025-01-27 08:32:38 -1000

    1-27

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import Project1button from "./project1button";
 
 export default function HomePage() {
   return (
+    <div>
     <p className="text-sl">Your projects</p>
-    
+    <div>
+      <Project1button></Project1button>
+    </div>
+    </div>
   );
 }
---
+++ src/app/project1button.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Project1button: React.FC = () => {
+    return (
+        <Link href = "/project1">
+            <button>Project 1</button>
+        </Link>
+    );
+};
+
+export default Project1button;
