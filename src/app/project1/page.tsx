2adb0b2ca4bcbb0845f29dc1f7d2c0d2c6d78911
Michael <2121401254@k12.hi.us>
2025-03-03 08:20:18 -1000

    3-3

--- src/app/project1/page.tsx
+++ src/app/project1/page.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
+import * as Plotly from 'plotly.js';
 
 export default function HomePage() {
-  return (
-    <div>
-    <p>Shut UP</p>
-    </div>
-  );
-}
+  const xArray = ["Italy","France","Spain","USA","Argentina"];
+  const yArray = [55, 49, 44, 24, 15];
+  
+  const data = [{
+    x: xArray,
+    y: yArray,
+    type: "bar",
+    orientation:"v",
+    marker: {color:"rgba(0,0,255)"}
+  }];
+  
+  const layout = {title:"World Wide Wine Production"};
+
+  return (Plotly.newPlot("myPlot", data, layout));
+};
